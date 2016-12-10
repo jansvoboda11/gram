97506d909b541b3585a837d17d9c4647f320b3ca
Jan Svoboda <jansvoboda11@gmail.com>
2016-12-10 21:30:56 +0100

    Give up unit testing "glue classes"

--- test/unit/population/processor/processor_test.cpp
+++ test/unit/population/processor/processor_test.cpp
@@ -1,23 +1,9 @@
 #include <gtest/gtest.h>
 
 #include <gram/population/processor/Processor.h>
-#include <gram/individual/MapperMock.h>
-#include <gram/language/EvaluatorMock.h>
-#include <gram/evolution/fitness_calculator/FitnessCalculatorMock.h>
 
-using namespace gram::grammar;
-using namespace gram::language;
-using namespace gram::individual;
 using namespace gram::population;
-using namespace gram::evolution;
 
 TEST(processor_test, test_something) {
-  auto startSymbol = std::make_shared<NonTerminal>();
-  gram::grammar::Grammar grammar(startSymbol);
-
-  MapperMock mapper(grammar);
-  EvaluatorMock evaluator;
-  FitnessCalculatorMock calculator;
-
-  Processor processor(mapper, evaluator, calculator);
+  //
 }
