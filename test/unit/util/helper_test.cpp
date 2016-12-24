7c25b0ee75fb796e5c9fca79a2183af5cd2fdd75
Jan Svoboda <jansvoboda11@gmail.com>
2016-12-24 13:48:40 +0100

    Add another test case for explode

--- test/unit/util/helper_test.cpp
+++ test/unit/util/helper_test.cpp
@@ -20,3 +20,13 @@ TEST(helper_test, test_it_does_not_split_string_without_delimiter) {
   ASSERT_EQ(1, pieces.size());
   ASSERT_EQ("hello world", pieces[0]);
 }
+
+TEST(helper_test, test_it_handles_multiple_delimiters_in_row) {
+  std::vector<std::string> pieces = explode("hello world", "l");
+
+  ASSERT_EQ(4, pieces.size());
+  ASSERT_EQ("he", pieces[0]);
+  ASSERT_EQ("", pieces[1]);
+  ASSERT_EQ("o wor", pieces[2]);
+  ASSERT_EQ("d", pieces[3]);
+}
