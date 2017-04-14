493afc3302dd13dcd8f59088f38c53e51315a7a3
Jan Svoboda <jansvoboda11@gmail.com>
2017-04-14 18:08:08 +0200

    Remove debug code

--- src/language/mapper/ContextFreeMapper.cpp
+++ src/language/mapper/ContextFreeMapper.cpp
@@ -8,28 +8,7 @@ ContextFreeMapper::ContextFreeMapper(shared_ptr<ContextFreeGrammar> grammar, uns
   //
 }
 
-string to_string(stack<Symbol*> st) {
-  string result;
-
-  while (!st.empty()) {
-    Symbol* sm = st.top();
-    st.pop();
-
-    if (sm->isTerminal()) {
-      result += static_cast<Terminal*>(sm)->getValue();
-    } else {
-      result += "<" + static_cast<NonTerminal*>(sm)->toRule().getName() + ">";
-    }
-  }
-
-  return result;
-}
-
 Phenotype ContextFreeMapper::map(const Genotype& genotype) {
-  string st;
-
-  ContextFreeGrammar& gr = *grammar.get();
-
   Phenotype phenotype;
   stack<Symbol*> symbols;
 
@@ -42,15 +21,9 @@ Phenotype ContextFreeMapper::map(const Genotype& genotype) {
   pushOption(symbols, startOption);
   codonIndex += 1;
 
-  st = to_string(symbols);
-
   while (!symbols.empty()) {
-    st = to_string(symbols);
-
     auto symbol = symbols.top();
     symbols.pop();
-
-    st = to_string(symbols);
 
     if (symbol->isTerminal()) {
       auto terminal = dynamic_cast<Terminal*>(symbol);
