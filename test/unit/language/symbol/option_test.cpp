c7597e118278d806c48af9a1aa6d967ec0ea642a
Jan Svoboda <jansvoboda11@gmail.com>
2017-04-17 12:58:15 +0200

    Improve option tests

--- test/unit/language/symbol/option_test.cpp
+++ test/unit/language/symbol/option_test.cpp
@@ -2,12 +2,15 @@
 
 #include <catch.hpp>
 
+#include "gram/language/symbol/NonTerminal.h"
+#include "gram/language/symbol/Option.h"
 #include "gram/language/symbol/Rule.h"
+#include "gram/language/symbol/Terminal.h"
 
 using namespace gram;
 using namespace std;
 
-TEST_CASE("option accepts a terminal", "[option]") {
+TEST_CASE("option accepts terminals", "[option]") {
   Terminal terminal1("first");
   Terminal terminal2("second");
 
@@ -19,6 +22,10 @@ TEST_CASE("option accepts a terminal", "[option]") {
   option.addTerminal(move(terminal2Ptr));
 
   REQUIRE(option.size() == 2);
+  REQUIRE(option[0].isTerminal());
+  REQUIRE(option[0].toTerminal().getValue() == "first");
+  REQUIRE(option[1].isTerminal());
+  REQUIRE(option[1].toTerminal().getValue() == "second");
 }
 
 TEST_CASE("option accepts a non-terminal", "[option]") {
@@ -30,13 +37,15 @@ TEST_CASE("option accepts a non-terminal", "[option]") {
   option.addNonTerminal(move(nonTerminal));
 
   REQUIRE(option.size() == 1);
+  REQUIRE(option[0].isNonTerminal());
+  REQUIRE(option[0].toNonTerminal().toRule().getName() == "rule");
 }
 
 TEST_CASE("option accepts both terminals and non-terminals", "[option]") {
   Rule rule("rule");
   auto nonTerminal = make_unique<NonTerminal>(rule);
 
-  auto terminal = make_unique<Terminal>("regular");
+  auto terminal = make_unique<Terminal>("terminal");
 
   Option option;
 
@@ -44,4 +53,8 @@ TEST_CASE("option accepts both terminals and non-terminals", "[option]") {
   option.addNonTerminal(move(nonTerminal));
 
   REQUIRE(option.size() == 2);
+  REQUIRE(option[0].isTerminal());
+  REQUIRE(option[0].toTerminal().getValue() == "terminal");
+  REQUIRE(option[1].isNonTerminal());
+  REQUIRE(option[1].toNonTerminal().toRule().getName() == "rule");
 }
