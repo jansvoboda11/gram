e5398e618925d3ed445a8764a3c7ed024c0b5cc7
Jan Svoboda <jansvoboda11@gmail.com>
2017-04-17 18:38:05 +0200

    Add includes to passionate reproducer

--- src/population/reproducer/PassionateReproducer.cpp
+++ src/population/reproducer/PassionateReproducer.cpp
@@ -1,7 +1,13 @@
 #include "gram/population/reproducer/PassionateReproducer.h"
 
 #include <algorithm>
+#include <memory>
 
+#include "gram/individual/Individual.h"
+#include "gram/individual/crossover/Crossover.h"
+#include "gram/individual/mutation/Mutation.h"
+#include "gram/population/Individuals.h"
+#include "gram/population/reproducer/Reproducer.h"
 #include "gram/population/selector/IndividualSelector.h"
 
 using namespace gram;
