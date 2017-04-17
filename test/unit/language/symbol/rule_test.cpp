583e86a05a65956ddfc5ab4d8a8530ce725a5396
Jan Svoboda <jansvoboda11@gmail.com>
2017-04-17 13:06:20 +0200

    Write grammar rule tests

--- test/unit/language/symbol/rule_test.cpp
+++ test/unit/language/symbol/rule_test.cpp
@@ -2,6 +2,29 @@
 
 #include <catch.hpp>
 
-TEST_CASE("test it does something", "[rule]") {
-  // todo: write tests
+#include "gram/error/InvalidGrammar.h"
+#include "gram/language/symbol/Option.h"
+#include "gram/language/symbol/Terminal.h"
+
+using namespace gram;
+using namespace std;
+
+TEST_CASE("rule requires a name", "[rule]") {
+  REQUIRE_THROWS_AS(Rule(""), InvalidGrammar);
+}
+
+TEST_CASE("rule accepts an option", "[rule]") {
+  auto terminal = make_unique<Terminal>("terminal");
+
+  auto option = make_unique<Option>();
+  option->addTerminal(move(terminal));
+
+  Rule rule("rule");
+
+  rule.addOption(move(option));
+
+  REQUIRE(rule.size() == 1);
+  REQUIRE(rule[0].size() == 1);
+  REQUIRE(rule[0][0].isTerminal());
+  REQUIRE(rule[0][0].toTerminal().getValue() == "terminal");
 }
